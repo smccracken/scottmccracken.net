b2f4f9a3b87d471cbeb2ae75d80bf2237eff82e1
Scott McCracken <86990+smccracken@users.noreply.github.com>
2026-02-14 17:57:19 -0500

    Base astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
+  prefetch: true,
+  site: "https://scottmccracken.net",
+  trailingSlash: "never",
+});
