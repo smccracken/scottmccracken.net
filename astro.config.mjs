b6bd66456d38b538d407ff0d0126730f02b016a8
Scott McCracken <86990+smccracken@users.noreply.github.com>
2026-02-15 12:44:11 -0500

    Enable astro fonts API

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 export default defineConfig({
   devToolbar: {
     enabled: false,
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: "Lora",
+        cssVariable: "--font-sans",
+        weights: ["400 700"],
+        fallbacks: ["sans-serif"],
+      },
+    ],
   },
   prefetch: true,
   site: "https://scottmccracken.net",
