2e0d4aba754de00c0a45918d0edcc465d62d5fee
Scott McCracken <86990+smccracken@users.noreply.github.com>
2026-02-16 22:11:26 -0500

    Setup light dark theme support

---
+++ src/scripts/theme-toggle.js
@@ -0,0 +1,39 @@
+const storageKey = "theme-preference";
+
+const onClick = () => {
+  theme.value = theme.value === "light" ? "dark" : "light";
+  setPreference();
+};
+
+const getColorPreference = () => {
+  if (localStorage.getItem(storageKey)) return localStorage.getItem(storageKey);
+  else return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+};
+
+const setPreference = () => {
+  localStorage.setItem(storageKey, theme.value);
+  reflectPreference();
+};
+
+const reflectPreference = () => {
+  document.firstElementChild.setAttribute("data-theme", theme.value);
+  document.querySelector("#theme-toggle")?.setAttribute("aria-label", theme.value);
+};
+
+const theme = {
+  value: getColorPreference(),
+};
+
+// run early
+reflectPreference();
+
+window.onload = () => {
+  reflectPreference();
+  document.querySelector("#theme-toggle").addEventListener("click", onClick);
+};
+
+// watch for system changes
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", ({ matches: isDark }) => {
+  theme.value = isDark ? "dark" : "light";
+  setPreference();
+});
